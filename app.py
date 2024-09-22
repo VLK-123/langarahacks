5dba74e03ac60571d988acd39ac877f7475e480d
VLK-123 <lalithkrishna.v@gmail.com>
2024-09-22 14:27:52 -0700

    last

--- app.py
+++ app.py
@@ -26,6 +26,26 @@ def index():
 def todo_task():
     return render_template('todo.html')
 
+@app.route('/calender')
+@cross_origin()
+def calender():
+    return render_template('calender.html')
+
+@app.route('/clock')
+@cross_origin()
+def clock():
+    return render_template('clock.html')
+
+@app.route('/stopwatch')
+@cross_origin()
+def stopwatch():
+    return render_template('stopwatch.html')
+
+@app.route('/weather')
+@cross_origin()
+def weather():
+    return render_template('weather.html')
+
 @app.route('/get_url/<int:id>', methods=['POST', 'GET'])
 @cross_origin()
 def get_url(id):
