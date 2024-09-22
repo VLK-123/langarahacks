170f774749fffac27ffd74129d67c3f8168ecf4a
VLK-123 <lalithkrishna.v@gmail.com>
2024-09-22 13:58:41 -0700

    project

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, session as flask_session, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+app.secret_key = '2024hacks'
+
+url_list = {
+    'site1' : '/get-url/1',
+    'site2' : '/get-url/2',
+    'site3' : '/get-url/3',
+    'site4' : '/get-url/4',
+    'site5' : '/get-url/5',
+    'site6' : '/get-url/6'
+}
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+@cross_origin()
+def index():
+    return render_template('index.html')
+
+@app.route('/todo_task')
+@cross_origin()
+def todo_task():
+    return render_template('todo.html')
+
+@app.route('/get_url/<int:id>', methods=['POST', 'GET'])
+@cross_origin()
+def get_url(id):
+    if request.method == 'POST':
+        url = request.form.get('url')
+        return redirect(url)
+    return render_template('url.html', id=id)
+
+if __name__ == '__main__':
+    app.run(debug=True)
